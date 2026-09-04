4b40742df45feec4fe197e30816ead4d209a5aea
jeevitham567-arch <jeevitham567@gmail.com>
2026-09-04 23:02:13 +0530

    Fix Product model error

--- croma-backend/models/product.js
+++ croma-backend/models/product.js
@@ -6,22 +6,27 @@ const productSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+
     description: {
       type: String,
       required: true,
     },
+
     price: {
       type: Number,
       required: true,
     },
+
     image: {
       type: String,
       required: true,
     },
+
     category: {
       type: String,
       required: true,
     },
+
     stock: {
       type: Number,
       default: 0,
@@ -32,4 +37,7 @@ const productSchema = new mongoose.Schema(
   }
 );
 
-module.exports = mongoose.model("Product", productSchema);
+const Product =
+  mongoose.models.Product || mongoose.model("Product", productSchema);
+
+module.exports = Product;
